f31df87fc0da8c6ee6c60b6e67e73509d0fd09df
Mirthe Valentijn <me@mirthe.org>
2024-02-24 16:01:13 +0100

    Correction in sortorder

--- snippets/getcurrentbooks.php
+++ snippets/getcurrentbooks.php
@@ -1,7 +1,7 @@
 <?php
     $localfile =  __DIR__ . "/goodreads-current.xml";
     $feedurl = "https://www.goodreads.com/review/list_rss/".option('mirthe.mygoodreads.accountnr').
-        "?key=".option('mirthe.mygoodreads.apiKey')."&shelf=currently-reading&sort=date_added&order=d";
+        "?key=".option('mirthe.mygoodreads.apiKey')."&shelf=currently-reading&sort=date_started&order=d";
     include_once ('storefile.php');
     $rss = simplexml_load_string($feeds);
     
